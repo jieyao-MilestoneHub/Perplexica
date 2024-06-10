95403954852b2fbce9a7d43a30635fd80688b29f
Jiao <yhocotw31016@gmail.com>
2024-06-11 01:46:10 +0800

    IInitial fix for Perplexica

---
+++ ui/app/library/page.tsx
@@ -0,0 +1,13 @@
+// ui/app/library/page.tsx
+import React from 'react';
+
+const LibraryPage = () => {
+  return (
+    <div>
+      <h1>Library Page</h1>
+      <p>This is the library page content.</p>
+    </div>
+  );
+};
+
+export default LibraryPage;
